d5020683bd01846dc700f37f5df45223b91c94f3
barrosotadeu <barrosotadeu3105@gmail.com>
2023-06-25 19:46:08 -0300

    function game completed

--- script.js
+++ script.js
@@ -16,36 +16,80 @@ function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase()
 
     if(playerSelection == "rock" && computerSelection == "paper"){
-        return "You lose! Paper beats rock"
+        console.log("You lose! Paper beats rock")
+        return "computer"
+        
     }
 
     if(playerSelection == "rock" && computerSelection == "scissors"){
-        return "You win! Rock beats Scissors"
+        console.log("You win! Rock beats Scissors")
+        return "player"
+        
     }
 
     if(playerSelection == "paper" && computerSelection == "rock"){
-        return "You win! Paper beats rock"
+        console.log("You win! Paper beats rock")
+        return "player"
+
     }
 
     if(playerSelection == "paper" && computerSelection == "scissors"){
-        return "You lose! Scissors beats paper"
+        console.log("You lose! Scissors beats paper")
+        return "computer"
     }
 
     if(playerSelection == "scissors" && computerSelection == "rock"){
-        return "You lose! Rock beats Scissors"
+        console.log("You lose! Rock beats Scissors")
+        return "computer"
     }
 
     if(playerSelection == "scissors" && computerSelection == "paper"){
-        return "You win! Scissors beats paper"
+        console.log("You win! Scissors beats paper")
+        return "player"
     }
 
     if(playerSelection == computerSelection){
-        return `Both players choose ${playerSelection}! It's a draw`
-    }  
+        console.log(`Both players choose ${playerSelection}! It's a draw`)
+    }     
 
 }
-let computerChoice = getComputerChoice();
-console.log(computerChoice)
-let playerChoice = "ROCK"
-console.log(playRound(playerChoice, computerChoice))
+
+
+
+
+function game(){
+    let round = 1
+    let computerVictories = 0
+    let playerVictories = 0
+    while(round <= 5){
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("Choose Rock, Paper or Scissor: ")
+        
+        let result = playRound(playerSelection, computerSelection)        
+        if(result == "player"){
+            playerVictories++
+        } else if(result == "computer"){
+            computerVictories++
+        }
+        
+        round++      
+    }
+    if(playerVictories > computerVictories){
+        console.log(`You had ${playerVictories} victories and the computer had ${computerVictories} victories. You win!`)
+    }
+    else if(computerVictories > playerVictories){
+        console.log(`You had ${playerVictories} victories and the computer had ${computerVictories} victories. You lose!`)
+    }else {
+        console.log(`You had ${playerVictories} victories and the computer had ${computerVictories} victories. Its a draw!`)
+
+    }
+    
+}
+
+game()
+
+
+
+
+
 
